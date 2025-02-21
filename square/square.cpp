58163523ea9b2a757328ce73a204974b59d91327
Влад Лисовский <lisovskij.vlad06@gmail.com>
2025-02-21 17:03:26 +0700

    Создал программу по задаче 1а

---
+++ square/square.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+#include <stdio.h>
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    printf("Введите высоту\n");
+    int h;
+    std::cin >> h;
+    printf("Введите длинну\n");
+    int l;
+    std::cin >> l;
+    int P = h * 2 + l * 2; int S = h * l; float diag = pow(h * h + l * l, 0.5);
+    printf("Периметр = %d\nПлощадь = %d\nДлинна диагонали = %.2f", P, S, diag);
+}
